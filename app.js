c37d0e318c03da041b5f4fe2d26e623ab2fb834b
Benny Peckruhn <benny@bennyville.de>
2020-12-27 16:25:07 +0100

    scope matrix var

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let codes = ['1C', '55', '7A', 'BD', 'E9'];
 
-let matrix = [];
 let puffer;
 let daemons;
 
 function genMatrix(lineLength) {
+    let matrix = [];
+
     for(let i = 0; i < lineLength; i++) {
-            matrix[i] = [];
+        matrix[i] = [];
+
         for(let j = 0; j < lineLength; j++) {
             matrix[i].push(codes[Math.floor(Math.random() * codes.length)]);
         }
