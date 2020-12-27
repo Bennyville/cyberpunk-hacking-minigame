5879b173cc96444bba33054e3bcf6b5b5b38a4c6
Benny Peckruhn <benny@bennyville.de>
2020-12-27 15:00:45 +0100

    generate matrix

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+let codes = ['1C', '55', '7A', 'BD', 'E9'];
+
+let matrix = [];
+let puffer;
+let daemons;
+
+function genMatrix(lineLength) {
+    for(let i = 0; i < lineLength; i++) {
+            matrix[i] = [];
+        for(let j = 0; j < lineLength; j++) {
+            matrix[i].push(codes[Math.floor(Math.random() * codes.length)]);
+        }
+    }
+
+    return matrix;
+}
+
+console.log(genMatrix(5));
 
